1f3cfb877b8e4d2f2c51129567ecfa33effeec3c
KuznecovAndrey7777777 <107943133+KuznecovAndrey7777777@users.noreply.github.com>
2024-03-16 14:48:34 +0200

    save1603241448

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,53 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+import HttpError from "../helpers/HttpError.js";
 
-export const getOneContact = (req, res) => {};
+import ctrlWrapper from "../decorators/ctrlWrapper.js";
 
-export const deleteContact = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+    const result = await contactsService.contactsList();
+    res.json(result);
+};
 
-export const createContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactsService.getContactById(id);
+    if (!result) {
+        throw HttpError(404);
+    }
+    res.json(result);
+};
 
-export const updateContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+    if (!result) {
+        throw HttpError(404);
+    }
+
+    res.status(204).send();
+};
+
+export const createContact = async (req, res) => {
+    const result = await contactsService.addContact(req.body);
+
+    res.status(201).json(result);
+};
+
+export const updateContact = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactsService.updateContactById(id, req.body);
+    if (!result) {
+        throw HttpError(404);
+    }
+
+    res.json(result);
+};
+
+export default {
+    getAllContacts: ctrlWrapper(getAllContacts),
+    getOneContact: ctrlWrapper(getOneContact),
+    deleteContact: ctrlWrapper(deleteContact),
+    createContact: ctrlWrapper(createContact),
+    updateContact: ctrlWrapper(updateContact),
+};
