c72373e6c5bb1ae50902be443cc0d7f0e1b4c1bf
KuznecovAndrey7777777 <107943133+KuznecovAndrey7777777@users.noreply.github.com>
2024-03-26 02:05:35 +0200

    s Schema

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,15 +4,16 @@ export const createContactSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     phone: Joi.string().required(),
-    favorite: Joi.boolean().optional()
+    favorite: Joi.boolean().optional().required()
 });
 
 export const updateContactSchema = Joi.object({
     name: Joi.string(),
-    email: Joi.string(),
+    email: Joi.string().email(),
     phone: Joi.string(),
+    favorite: Joi.boolean().optional()
 });
 
 export const favoriteSchema = Joi.object({
-    favorite: Joi.boolean().required(),
+    favorite: Joi.boolean().required()
 });
