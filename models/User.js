20d336c6897607fc4d6445c72660e43b35c333d0
KuznecovAndrey7777777 <107943133+KuznecovAndrey7777777@users.noreply.github.com>
2024-04-07 23:27:46 +0300

    save0704242327

--- models/User.js
+++ models/User.js
@@ -29,11 +29,8 @@ const userSchema = new Schema(
 );
 
 userSchema.pre('findOneAndUpdate', setUpdateSetting);
-
 userSchema.post('save', handleSaveError);
-
 userSchema.post('findOneAndUpdate', handleSaveError);
-
 const User = model('user', userSchema);
 
 export default User;
--- services/contactsServices.js
+++ services/contactsServices.js
@@ -4,15 +4,10 @@ const listContacts = (filter = {}, setting = {}) =>
     Contact.find(filter, '-createdAt -updatedAt', setting).populate('owner', 'email subscription');
 
 const countContacts = filter => Contact.countDocuments(filter);
-
 const addContact = data => Contact.create(data);
-
 const getContactByFilter = filter => Contact.findOne(filter);
-
 const updateContactByFilter = (filter, data) => Contact.findOneAndUpdate(filter, data);
-
 const removeContactByFilter = filter => Contact.findOneAndDelete(filter);
-
 const updateContactStatusByFilter = (filter, data) => Contact.findOneAndUpdate(filter, data);
 
 export default {
